b21712be3003fa74bfd0cdfa0cfccb92e6f9389e
USER <jp.pesantezt@uea.edu.ec>
2026-01-16 23:15:36 -0500

    Agregar clase Nodo y programa principal

---
+++ EjercicioLisenlazada/Program.cs
@@ -0,0 +1,27 @@
+﻿// Implementación de un nodo
+
+public class Nodo
+{
+    public int Data;
+    public Nodo? Next;
+
+    public Nodo(int data)
+    {
+        this.Data = data;
+        this.Next = null;
+    }
+}
+
+// Programa principal
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Programa de Lista Enlazada");
+        
+        // Crear un nodo de prueba
+        Nodo nodo1 = new Nodo(10);
+        Console.WriteLine($"Nodo creado con valor: {nodo1.Data}");
+    }
+}
+
